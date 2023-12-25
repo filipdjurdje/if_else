ac824b87db8243a43a7d7fabc71fde6640e5dddc
unknown <filipdjurdjevac12356@gmail.com>
2023-12-25 16:10:20 +0100

    first commit

---
+++ src/zadatak16/If_else_domasni.java
@@ -0,0 +1,31 @@
+package zadatak16;
+
+public class If_else_domasni {
+
+	public class If_else_domasno {
+
+		public static void main(String[] args) {
+			int age = 25;
+			if(age > 0)
+			{
+				if( age > 16 )
+				{
+					System.out.println("Welcome!");
+				}
+				else
+				{
+					System.out.println("Too Young !");
+				}
+				
+			}
+			else 
+			{
+				System.out.println("Error");
+			}
+			// Outputs "Welcome!"
+
+		}
+
+	}
+}
+
